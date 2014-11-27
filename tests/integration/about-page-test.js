ec45987441004c07f22c7212aac40f2bf4547313
Likun <hu.likun@gmail.com>
2014-11-26 21:31:47 -0500

    added integration tests for about page

---
+++ tests/integration/about-page-test.js
@@ -0,0 +1,29 @@
+import startApp from 'feapp/tests/helpers/start-app';
+import Ember from 'ember';
+
+var App;
+
+module('Integration - About Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should navigate to the About page', function(){
+  visit('/').then(function() {
+    click("a:contains('About')").then(function() {
+      equal(find('h3').text(), 'About');
+    });
+  });
+});
+
+test('Should allow navigating back to root from another page', function() {
+  visit('/about').then(function() {
+    click('a:contains("home")').then(function() {
+      notEqual(find('h3').text(), 'About');
+    });
+  });
+});
