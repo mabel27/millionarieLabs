598b23559a73ed4f7f7425da051c5ab8fa80f113
Likun <hu.likun@gmail.com>
2014-12-04 19:13:25 -0500

    Added Bootstrap bower_component. Use less compiler for style preprocessor.

--- Brocfile.js
+++ Brocfile.js
@@ -1,14 +1,24 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
 
 var app = new EmberApp();
+var bootstrapFonts = pickFiles('bower_components/bootstrap/dist/fonts', {
+  srcDir: '/',
+  files: ['**/*'],
+  destDir: '/fonts'
+});
+
+
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
 
-// Tell Broccoli that we want this file to be concatenated with our vendor.css
+// Tell Broccoli that we want this file to be concatenated with our vendor.css and vendor.js
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
@@ -20,4 +30,6 @@ app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+var outputTrees = [app.toTree()]
+
+module.exports = mergeTrees([app.toTree(), bootstrapFonts]);
